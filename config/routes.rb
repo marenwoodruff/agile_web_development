3a1f03373d9ad7c22736829d1ee33bb2e311c4bb
Maren Woodruff <wren403@gmail.com>
2015-05-13 08:46:01 -0400

    added playtime work

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'say/goodbye'
 
+  get 'say/file'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
